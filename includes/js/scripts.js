31f25a3d6b941ed78af8a664d2ae1cd13e1d253b
r1bb3t <r1bb3t@gmail.com>
2017-05-30 22:11:28 -0700

    updated script to work with new layout

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -17,12 +17,53 @@ $(document).ready(function(){
   // betting page ==============================================================
 
   // on click change background
-  $('[class^="fight-"]').on('click', function(event){
+  // $('[class^="fight-"]').on('click', function(event){
+  //   $(this).addClass("bet-fighter-selected").siblings().removeClass("bet-fighter-selected");
+  //
+  //   var target_id = event.target.id;
+  //   var split = target_id.split('-');
+  //   var fight_id = split[0];
+  //   var selection = split[1];
+  //   console.log('target_id: ' + target_id);
+  //   console.log('fight_id: ' + fight_id);
+  //   console.log('selection: ' + selection);
+  //   $('input[id=' + fight_id + ']').val(selection);
+  // });
+
+  $('[class^="col-xs-5 fight-"]').on('click', function(event){
     $(this).addClass("bet-fighter-selected").siblings().removeClass("bet-fighter-selected");
-    var target_id = event.target.id;
+
+    //alert();
+
+    var target_id = this.id;
     var split = target_id.split('-');
     var fight_id = split[0];
     var selection = split[1];
+    console.log('target_id: ' + target_id);
+    console.log('fight_id: ' + fight_id);
+    console.log('selection: ' + selection);
     $('input[id=' + fight_id + ']').val(selection);
   });
+
+  // $('[class^="fight-"]').on('click', function(event){
+  //   $(this).addClass("bet-fighter-selected").siblings().removeClass("bet-fighter-selected");
+  //
+  //   var target_id = event.target.id;
+  //   var split = target_id.split('-');
+  //   var fight_id = split[0];
+  //   var selection = split[1];
+  //   console.log('target_id: ' + target_id);
+  //   console.log('fight_id: ' + fight_id);
+  //   console.log('selection: ' + selection);
+  //   $('input[id=' + fight_id + ']').val(selection);
+  // });
+
+  // $('[class^="fight-"]').on('click', function(event){
+  //   $(this).addClass("bet-fighter-selected");
+  //   var target_id = event.target.id;
+  //   var split = target_id.split('-');
+  //   var fight_id = split[0];
+  //   var selection = split[1];
+  //   $('input[id=' + fight_id + ']').val(selection);
+  // });
 });
