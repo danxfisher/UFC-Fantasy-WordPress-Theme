8c57be507323ab0b8c7c372807b61f0dfbb94e8e
r1bb3t <r1bb3t@gmail.com>
2017-05-30 22:12:27 -0700

    updated styles for betting page

--- page-betting.php
+++ page-betting.php
@@ -150,118 +150,113 @@ if ($ufc_event_id && $event_title) {
       </div>
       <?php if ($bets_update_success) { ?>
       <div id="ufc-bets-success">
-        <div class="row">
-          <div class="col-md-3">
-            <!-- nothing -->
-          </div>
-          <div class="col-md-6">
-            <div class="bets-success-message">
-              <div id="close-bet-success"></div>
-              <?php echo $bets_update_success_message; ?>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="bets-success-message">
+                <div id="close-bet-success"></div>
+                <?php echo $bets_update_success_message; ?>
+              </div>
             </div>
-          </div>
-          <div class="col-md-3">
-            <!-- nothing -->
           </div>
         </div>
       </div>
       <?php } ?>
 
       <div id="ufc-bets">
-      <?php if (is_user_logged_in()) {
-        foreach ($event as $ev) {
-          $event_lock_time = $ev->lock_time;
-        }
-        echo '<br />';
-        date_default_timezone_set('America/Los_Angeles');
+          <div class="container">
+        <?php if (is_user_logged_in()) {
+          foreach ($event as $ev) {
+            $event_lock_time = $ev->lock_time;
+          }
+          echo '<br />';
+          date_default_timezone_set('America/Los_Angeles');
 
-        $current_time = date('Y-m-d H:i:s');
+          $current_time = date('Y-m-d H:i:s');
 
-        if (strtotime($event_lock_time) > strtotime($current_time)) {
-        ?>
-        <form action="?action=add-bets&amp;ufc_event_id=<?php echo $ufc_event_id ?>&amp;title=<?php echo $event_title ?>" method="POST" id="add-bets">
-          <div class="row">
-            <div class="col-md-4">
-              <!-- nothing -->
-            </div>
-            <div class="col-md-4 bet-fights">
-              <?php foreach ($fights_order as $fight) { ?>
-                <input type="hidden" name="<?php echo $fight->id; ?>" id="<?php echo $fight->id; ?>" value="<?php $betValue = 0;foreach($bets as $bet){if ($bet->fight_id == $fight->id){if ($bet->fighter_selected == $fight->fighter1){$betValue = 'fighter1';}else{$betValue = 'fighter2';}}}echo $betValue;?>">
-                <table>
-                  <tr>
-                    <td colspan="3" class="bet-fight-title">
+          if (strtotime($event_lock_time) > strtotime($current_time)) {
+          ?>
+          <form action="?action=add-bets&amp;ufc_event_id=<?php echo $ufc_event_id ?>&amp;title=<?php echo $event_title ?>" method="POST" id="add-bets">
+            <div class="row">
+              <!-- <div class="col-md-4"> -->
+                <!-- nothing -->
+              <!-- </div> -->
+
+              <div class="col-md-12 bet-fights">
+                <?php foreach ($fights_order as $fight) { ?>
+                  <div class="row">
+                    <div class="col-md-12 bet-fight-title">
                       <?php echo $fight->weight_class ?>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="40%" class="bet-img">
-                      <?php foreach ($ufc_event as $event_fight) {
-                        if ($fight->ufc_fight_id == $event_fight->id) {
-                      ?>
-                          <img src="<?php echo $event_fight->fighter1_profile_image; ?>" class="img-responsive" />
-                        <?php }
-                      } ?>
-                    </td>
-                    <td width="20%">
-                      &#160;
-                    </td>
-                    <td width="40%" class="bet-img">
-                      <?php foreach ($ufc_event as $event_fight) {
-                        if ($fight->ufc_fight_id == $event_fight->id) {
-                      ?>
-                          <img src="<?php echo $event_fight->fighter2_profile_image; ?>" class="img-responsive" />
-                        <?php }
-                      } ?>
-                    </td>
-                  </tr>
-                  <tr class="fight">
-                    <td width="40%" class="fight-<?php echo $fight->id ?> bet-fighter fighter-1<?php if ($betValue === 'fighter1'){echo ' bet-fighter-selected';}?>" id="<?php echo $fight->id ?>-fighter1">
-                      <?php echo $fight->fighter1; ?>
-                    </td>
-                    <td width="20%" class="bet-fighter-td">
-                      vs
-                    </td>
-                    <td width="40%" class="fight-<?php echo $fight->id ?> bet-fighter fighter-2<?php if ($betValue === 'fighter2'){echo ' bet-fighter-selected';}?>" id="<?php echo $fight->id ?>-fighter2">
-                      <?php echo $fight->fighter2; ?>
-                    </td>
-                  </tr>
-                </table>
-              <?php } ?>
+                    </div>
+                  </div>
+                  <input type="hidden" name="<?php echo $fight->id; ?>" id="<?php echo $fight->id; ?>" value="<?php $betValue = 0;foreach($bets as $bet){if ($bet->fight_id == $fight->id){if ($bet->fighter_selected == $fight->fighter1){$betValue = 'fighter1';}else{$betValue = 'fighter2';}}}echo $betValue;?>">
+                  <div class="row">
+                    <div class="col-xs-5 fight-<?php echo $fight->id ?> bet-fighter fighter-1<?php if ($betValue === 'fighter1'){echo ' bet-fighter-selected';}?>" id="<?php echo $fight->id ?>-fighter1">
+                      <?php foreach ($ufc_event as $event_fight): ?>
+                        <?php if ($fight->ufc_fight_id == $event_fight->id): ?>
+                          <img src="<?php echo $event_fight->fighter1_profile_image; ?>" class="img-responsive center-stuff" />
+                        <?php endif; ?>
+                      <?php endforeach; ?>
+                      <p class="bet__fighter-names">
+                        <?php echo $fight->fighter1; ?>
+                      </p>
+                    </div>
+                    <div class="col-xs-2">
+
+                    </div>
+                    <div class="col-xs-5 fight-<?php echo $fight->id ?> bet-fighter fighter-2<?php if ($betValue === 'fighter2'){echo ' bet-fighter-selected';}?>" id="<?php echo $fight->id ?>-fighter2">
+                      <?php foreach ($ufc_event as $event_fight): ?>
+                        <?php if ($fight->ufc_fight_id == $event_fight->id): ?>
+                          <img src="<?php echo $event_fight->fighter2_profile_image; ?>" class="img-responsive center-stuff" />
+                        <?php endif; ?>
+                      <?php endforeach; ?>
+                      <p class="bet__fighter-names">
+                        <?php echo $fight->fighter2; ?>
+                      </p>
+                    </div>
+                  </div>
+
+
+                
+
+
+                <?php } ?>
+              </div>
+              <!-- <div class="col-md-4"> -->
+                <!-- nothing -->
+              <!-- </div> -->
             </div>
-            <div class="col-md-4">
-              <!-- nothing -->
+            <div class="row">
+              <div class="col-md-12 bet-fights-submit">
+                <input type="submit" name="submit" id="add-bets-submit" value="Confirm Bets" />
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 bet-fights-submit">
-              <input type="submit" name="submit" id="add-bets-submit" value="Confirm Bets" />
+          </form>
+          <?php } else { ?>
+            <div class="row">
+              <div class="col-md-4">
+                <!-- nothing -->
+              </div>
+              <div class="col-md-4">
+                <div class="bet-locked">
+                  Betting for this event has been <span style="font-weight: bold;">locked.</span>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <!-- nothing -->
+              </div>
             </div>
-          </div>
-        </form>
+          <?php } ?>
         <?php } else { ?>
-          <div class="row">
-            <div class="col-md-4">
-              <!-- nothing -->
-            </div>
-            <div class="col-md-4">
-              <div class="bet-locked">
-                Betting for this event has been <span style="font-weight: bold;">locked.</span>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="bet-login-text">
+                  Please log in to select your picks.
+                </div>
               </div>
             </div>
-            <div class="col-md-4">
-              <!-- nothing -->
-            </div>
-          </div>
         <?php } ?>
-      <?php } else { ?>
-          <div class="row">
-            <div class="col-md-12">
-              <div class="bet-login-text">
-                Please log in to select your picks.
-              </div>
-            </div>
-          </div>
-      <?php } ?>
+        </div>
       </div>
 
 <?php
