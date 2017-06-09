3b4f4a5ee920b205d4772a2cffde2d3df94177b4
r1bb3t <r1bb3t@gmail.com>
2017-06-09 00:58:53 -0700

    removed cached files

--- includes/functions/woocommerce.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Add Woocommerce theme support
- */
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
-?>
