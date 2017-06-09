d1b65438e275dd60d0a2b984acb12b5f4f0b65e1
r1bb3t <r1bb3t@gmail.com>
2017-06-09 00:56:16 -0700

    rm some files and ignored others

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
 
-// theme support
-//require_once(get_template_directory().'/includes/functions/theme-support.php');
-
 // theme cleanup
 require_once(get_template_directory().'/includes/functions/cleanup.php');
 
@@ -26,32 +23,7 @@ if ($captcha_enabled == 'yes') {
   require_once(get_template_directory().'/includes/functions/captcha-forms.php');
 }
 
-// register admin styles
-// require_once(get_template_directory().'/includes/functions/enqueue-admin-styles.php');
-
-// register custom menus and menu walkers
-// require_once(get_template_directory().'/includes/functions/menu.php');
-// require_once(get_template_directory().'/includes/bootstrap-wp-navwalker.php');
-
-// register sidebars/widget areas
-// require_once(get_template_directory().'/includes/functions/sidebar.php');
-
-// register template tags
-// require_once(get_template_directory().'/includes/functions/template-tags.php');
-
-// adds multiple language support
-require_once(get_template_directory().'/includes/functions/languages.php');
-
 // adds multiple language support
 require_once(get_template_directory().'/includes/functions/advanced-custom-fields.php');
-
-// add woocommerce theme support
-// require_once(get_template_directory().'/includes/functions/woocommerce.php');
-
-// add custom footer builder post type
-// require_once(get_template_directory().'/includes/php/footer-builder/footer-builder.php');
-
-// add customizer options
-// require get_template_directory() . '/includes/customizer.php';
 
 ?>
