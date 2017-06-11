b79b75d52ce0116c92fc0cb7af0113fcc74e46d3
r1bb3t <r1bb3t@gmail.com>
2017-06-10 23:20:43 -0700

    updated fixes for 'upcoming fights' and 'results

--- index.php
+++ index.php
@@ -25,6 +25,8 @@
 
 <?php date_default_timezone_set('America/Los_Angeles'); ?>
 
+<?php $thedate = date('Y-m-d'); ?>
+
 <section id="s__events">
 	<div class="container">
 		<div class="row">
@@ -32,7 +34,7 @@
 				<h2>Upcoming Events</h2>
 				<div class="row event-list">
 
-				<?php $today = date('Y-m-d', time() - 86400); ?>
+				<?php $today = date('Y-m-d'); ?>
 				<?php $args = array(
 								'post_type'		=> 'post',
 								'meta_query'	=>  array (
@@ -51,10 +53,20 @@
 					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
 						<?php $event_id = get_the_title(); ?>
-							<div class="col-md-4 events__item">
-								<a href="<?php echo get_the_permalink(); ?>">
-									<?php foreach($events as $obj): ?>
-							      <?php if ($obj->id == $event_id): ?>
+
+						<?php foreach($events as $obj): ?>
+							<?php if ($obj->id == $event_id): ?>
+
+								<?php // calculations to see if event is 'live' or aka the current time is less than or equal to 3 hours from start ?>
+								<?php $ue_event_time = $obj->event_time_text ?>
+								<?php $ue_event_time = explode('/', $ue_event_time); ?>
+								<?php $ue_event_start_time = date('H', strtotime($ue_event_time[1])); ?>
+
+								<?php if (($thedate == get_field('event_start_date') && date('H') < $ue_event_start_time) || $thedate != get_field('event_start_date')) : ?>
+
+									<div class="col-md-4 events__item">
+										<a href="<?php echo get_the_permalink(); ?>">
+
 											<div class="col-sm-12 events__thumbnail-image" style="background-image:url('<?php echo $obj->feature_image ?>'); background-color: rgba(0,0,0,0.5);">
 												<div class="events__title-wrap">
 													<div class="events__title">
@@ -71,10 +83,13 @@
 													<?php echo date('F j, Y', strtotime(get_field('event_start_date'))); ?>
 												</div>
 											</div>
-									<?php endif; ?>
-									<?php endforeach; ?>
-								</a>
-							</div>
+
+										</a>
+									</div>
+
+								<?php endif; ?>
+							<?php endif; ?>
+						<?php endforeach; ?>
 
 					<?php endwhile; ?>
 				<?php else : ?>
@@ -99,9 +114,6 @@
 				<?php wp_reset_query(); ?>
 				<?php wp_reset_postdata(); ?>
 
-
-
-				<?php $thedate = date('Y-m-d'); ?>
 				<?php $args_results = array(
 								'post_type'		=> 'post',
 								'posts_per_page'	=>	'3',
@@ -121,37 +133,45 @@
 					<?php while ( $the_results_query->have_posts() ) : $the_results_query->the_post(); ?>
 
 						<?php $event_id = get_the_title(); ?>
-							<div class="col-md-4 events__item">
-								<a href="<?php echo get_the_permalink(); ?>">
-									<?php foreach($events as $obj): ?>
-							      <?php if ($obj->id == $event_id): ?>
-											<div class="col-sm-12 events__thumbnail-image" style="background-image:url('<?php echo $obj->feature_image ?>'); background-color: rgba(0,0,0,0.5);">
-												<div class="events__title-wrap">
-													<div class="events__title">
-														<?php the_field('event_title'); ?>
-													</div>
+
+						<?php foreach($events as $obj): ?>
+							<?php if ($obj->id == $event_id): ?>
+								<div class="col-md-4 events__item">
+									<a href="<?php echo get_the_permalink(); ?>">
+
+										<div class="col-sm-12 events__thumbnail-image" style="background-image:url('<?php echo $obj->feature_image ?>'); background-color: rgba(0,0,0,0.5);">
+											<div class="events__title-wrap">
+												<div class="events__title">
+													<?php the_field('event_title'); ?>
 												</div>
 											</div>
+										</div>
 
-											<?php // need to add a fix to compare hour to the hour from the API ?>
-											<?php if (($thedate == get_field('event_start_date')) && date('H') >= 19 && date('H') <= 23) : ?>
-												<div class="col-sm-12 results__live">
-													Live
-												</div>
-											<?php endif; ?>
+										<?php // calculations to see if event is 'live' or aka the current time is less than or equal to 3 hours from start ?>
+										<?php $r_event_time = $obj->event_time_text ?>
+										<?php $r_event_time = explode('/', $r_event_time); ?>
+										<?php $r_event_start_time = date('H', strtotime($r_event_time[1])); ?>
+										<?php $r_event_end_time = date('H', strtotime('+2 hours', strtotime($r_event_time[1]))); ?>
 
-											<div class="col-sm-12 events__date-wrap">
-												<div class="events__tagline">
-													<?php echo $obj->title_tag_line ?>
-												</div>
-												<div class="events__date">
-													<?php echo date('F j, Y', strtotime(get_field('event_start_date'))); ?>
-												</div>
+										<?php if (($thedate == get_field('event_start_date')) && date('H') >= $r_event_start_time && date('H') <= $r_event_end_time) : ?>
+											<div class="col-sm-12 results__live">
+												Live
 											</div>
-									<?php endif; ?>
-									<?php endforeach; ?>
-								</a>
-							</div>
+										<?php endif; ?>
+
+										<div class="col-sm-12 events__date-wrap">
+											<div class="events__tagline">
+												<?php echo $obj->title_tag_line ?>
+											</div>
+											<div class="events__date">
+												<?php echo date('F j, Y', strtotime(get_field('event_start_date'))); ?>
+											</div>
+										</div>
+
+									</a>
+								</div>
+							<?php endif; ?>
+						<?php endforeach; ?>
 
 					<?php endwhile; ?>
 				<?php endif; ?>
